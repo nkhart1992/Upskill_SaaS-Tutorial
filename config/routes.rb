14c16547d844197f178588d61be9c5dfc91bfc4a
Nick Hart <nk.hart1992@gmail.com>
2019-04-19 00:28:22 +0000

    added bootstrap gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,8 @@ Rails.application.routes.draw do
  root to: 'pages#home'
  get 'about', to: 'pages#about'
 end
+
+Rails.application.routes.draw do
+ root to: 'pages#home'
+ get 'faq', to: 'pages#faq'
+end
